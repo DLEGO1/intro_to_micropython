64eda23e6ac657e08a894b5f0fdca540ab6d017f
DLEGO1 <diego.logue-perron@waringschool.org>
2024-12-18 09:23:29 -0500

    Update blink.py

--- Output devices/LED/code/blink.py
+++ Output devices/LED/code/blink.py
@@ -9,7 +9,7 @@ import time		#library to handle time functions.
 
 #led_1 is created as an instance of the Class 'Pin'.  The first argument is the pin
 #number.  The second argument sets the function of the pin.
-led_1 = Pin(0, Pin.OUT)   
+led_1 = Pin(15, Pin.OUT)   
 
 while True:					#loop forever
     led_1.value(1)			#make the voltage on the pin high.
